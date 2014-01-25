dd95104e0953a1b44b5b06ef4bcce9241a9c3be3
Karen Etheridge <ether@cpan.org>
2014-01-25 18:18:00 +0000

    Import of ETHER/App-Software-License-0.02 from CPAN.
    gitpan-cpan-distribution: App-Software-License
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ETHER/App-Software-License-0.02.tar.gz
    gitpan-cpan-author:       ETHER
    gitpan-cpan-maturity:     released

--- lib/App/Software/License.pm
+++ lib/App/Software/License.pm
@@ -1,18 +1,44 @@
 package App::Software::License;
-our $VERSION = '0.01';
-
-# ABSTRACT: commandline interface to Software::License
+BEGIN {
+  $App::Software::License::AUTHORITY = 'cpan:ETHER';
+}
+# git description: v0.01-11-gd90315e
+$App::Software::License::VERSION = '0.02';
+# ABSTRACT: command-line interface to Software::License
 
 use Moose;
 use MooseX::Types::Moose qw/Str Num Maybe/;
 use File::HomeDir;
 use File::Spec::Functions qw/catfile/;
+use Module::Runtime qw/use_module/;
+use Software::License;
 
-use namespace::clean -except => 'meta';
+use namespace::autoclean;
 
 with qw/MooseX::Getopt MooseX::SimpleConfig/;
 
+# =head1 SYNOPSIS
+#
+#     software-license --holder 'J. Random Hacker' --license Perl_5 --type notice
+#
+# =head1 DESCRIPTION
+#
+# =for stopwords fulltext versioned
+#
+# This module provides a command-line interface to Software::License. It can be
+# used to easily produce license notices to be included in other documents.
+#
+# All the attributes documented below are available as command-line options
+# through L<MooseX::Getopt> and can also be configured in
+# L<$HOME/.software_license.conf> though L<MooseX::SimpleConfig>.
+#
+# =cut
 
+# =attr holder
+#
+# Name of the license holder.
+#
+# =cut
 
 has holder => (
     is       => 'ro',
@@ -20,6 +46,11 @@ has holder => (
     required => 1,
 );
 
+# =attr year
+#
+# Year to be used in the copyright notice.
+#
+# =cut
 
 has year => (
     is       => 'ro',
@@ -27,6 +58,12 @@ has year => (
     default  => undef,
 );
 
+# =attr license
+#
+# Name of the license to use. Must be the name of a module available under the
+# Software::License:: namespace. Defaults to Perl_5.
+#
+# =cut
 
 has license => (
     is      => 'ro',
@@ -34,6 +71,38 @@ has license => (
     default => 'Perl_5',
 );
 
+# =attr type
+#
+# The type of license notice you'd like to generate. Available values are:
+#
+# B<* notice>
+#
+# This method returns a snippet of text, usually a few lines, indicating the
+# copyright holder and year of copyright, as well as an indication of the license
+# under which the software is distributed.
+#
+# B<* license>
+#
+# This method returns the full text of the license.
+#
+# B<* fulltext>
+#
+# This method returns the complete text of the license, preceded by the copyright
+# notice.
+#
+# B<* version>
+#
+# This method returns the version of the license.  If the license is not
+# versioned, this returns nothing.
+#
+# B<* meta_yml_name>
+#
+# This method returns the string that should be used for this license in the CPAN
+# META.yml file, or nothing if there is no known string to use.
+#
+# =for Pod::Coverage run
+#
+# =cut
 
 has type => (
     is      => 'ro',
@@ -61,8 +130,7 @@ has _software_license => (
 sub _build__software_license {
     my ($self) = @_;
     my $class = "Software::License::${\$self->license}";
-    Class::MOP::load_class($class);
-    return $class->new({
+    return use_module($class)->new({
         holder => $self->holder,
         year   => $self->year,
     });
@@ -93,13 +161,20 @@ __PACKAGE__->meta->make_immutable;
 1;
 
 __END__
+
+=pod
+
+=encoding UTF-8
+
+=for :stopwords Florian Ragwitz Karen Etheridge Randy Stauner fulltext versioned
+
 =head1 NAME
 
-App::Software::License - commandline interface to Software::License
+App::Software::License - command-line interface to Software::License
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 SYNOPSIS
 
@@ -107,35 +182,20 @@ version 0.01
 
 =head1 DESCRIPTION
 
-This module provides a commandline interface to Software::License. It can be
-used to easily produce license notices to be included in other documents.
-
-All the attributes documented below are available as commandline options
-through L<MooseX::Getopt> and can also be configured in
-L<$HOME/.software_license.conf> though L<MooseX::SimpleConfig>.
-
-
-
 =head1 ATTRIBUTES
 
 =head2 holder
 
 Name of the license holder.
 
-
-
 =head2 year
 
 Year to be used in the copyright notice.
-
-
 
 =head2 license
 
 Name of the license to use. Must be the name of a module available under the
 Software::License:: namespace. Defaults to Perl_5.
-
-
 
 =head2 type
 
@@ -166,14 +226,38 @@ B<* meta_yml_name>
 This method returns the string that should be used for this license in the CPAN
 META.yml file, or nothing if there is no known string to use.
 
+This module provides a command-line interface to Software::License. It can be
+used to easily produce license notices to be included in other documents.
+
+All the attributes documented below are available as command-line options
+through L<MooseX::Getopt> and can also be configured in
+L<$HOME/.software_license.conf> though L<MooseX::SimpleConfig>.
+
+=for Pod::Coverage run
+
 =head1 AUTHOR
 
-  Florian Ragwitz <rafl@debian.org>
+Florian Ragwitz <rafl@debian.org>
 
 =head1 COPYRIGHT AND LICENSE
 
 This software is copyright (c) 2009 by Florian Ragwitz.
 
 This is free software; you can redistribute it and/or modify it under
-the same terms as perl itself.
+the same terms as the Perl 5 programming language system itself.
 
+=head1 CONTRIBUTORS
+
+=over 4
+
+=item *
+
+Karen Etheridge <ether@cpan.org>
+
+=item *
+
+Randy Stauner <rwstauner@cpan.org>
+
+=back
+
+=cut
